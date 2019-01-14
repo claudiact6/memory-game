baced23e9377ed536b6c667e112f22ae1a61f76b
kimk <38691828+claudiact6@users.noreply.github.com>
2019-01-13 21:20:29 -0600

    Updates

---
+++ precache-manifest.15abc2a0881b289aa28c73c01b992485.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "66c34b29d278a5d14e76",
+    "url": "/memory-game/static/js/runtime~main.66c34b29.js"
+  },
+  {
+    "revision": "06fdfdea07f4e8bf086e",
+    "url": "/memory-game/static/js/main.06fdfdea.chunk.js"
+  },
+  {
+    "revision": "aac75bbadb65a070ad02",
+    "url": "/memory-game/static/js/1.aac75bba.chunk.js"
+  },
+  {
+    "revision": "06fdfdea07f4e8bf086e",
+    "url": "/memory-game/static/css/main.d123cfa0.chunk.css"
+  },
+  {
+    "revision": "3809727e7af9453ea768f03fc07117f9",
+    "url": "/memory-game/index.html"
+  }
+];
